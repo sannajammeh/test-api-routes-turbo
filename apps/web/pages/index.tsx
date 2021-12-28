f44f405164abe0f94f9097890d138acb117c902a
Sanna Jammeh <sannajam@outlook.com>
2021-12-28 22:28:30 +0100

    Trigger depooyment

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Webs</h1>
+      <h1>Trigger deployment please</h1>
       <Button />
     </div>
   );
